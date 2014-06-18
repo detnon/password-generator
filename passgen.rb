ed527ad4216f2014c3f8d678d3191fa9fafd71d6
Samuel <samdetnon@gmail.com>
2014-06-19 00:53:59 +0100

    put everything into a method

--- passgen.rb
+++ passgen.rb
@@ -1,5 +1,12 @@
+
 PHRASES = %w( zh aw oo ow oi nk ng wh th sh ch str a e i o u ! % & £ $ )
 
-pass = PHRASES[rand(PHRASES.length)]
+	
 
-puts pass
+	def generate
+
+		pass = PHRASES[rand(PHRASES.length)]
+		return pass
+	end
+
+puts generate
