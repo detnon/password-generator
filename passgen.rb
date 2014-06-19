a0b7887669bdc14b4a6d1886805e9a5baa9caa82
Sam Detnon <sam.detnon@headlondon.com>
2014-06-19 19:12:37 +0100

    Added an ask method

--- passgen.rb
+++ passgen.rb
@@ -7,6 +7,8 @@ class Password
 
 	PHRASES = %w( zh aw oo ow oi nk ng wh th sh ch str a e i o u ! % & £ $ )
 
+# Generate takes an input and generates a password of that numetrical value
+
 		def generate(length)
 			result = ''
 
@@ -15,14 +17,38 @@ class Password
 				result += phra
 			end
 
+			# Enures the password will always be the specified length
 			result = result[0,length]
 			return result
 
 		end
+
+
+
+# Ask the user how many character the password should be.
+
+	def ask	
+
+		puts 'How many characters?'
+		size = gets.chomp.to_i
+
+
+		
+		
+
+
+	end
+
 end 
 
+p = Password.new
+p.ask
 
 
-	sec = Password.new
 
-	puts sec.generate(8)
+
+#	sec = Password.new
+#
+#	puts " "
+#	puts "Your Password is: " + sec.generate(8)
+#	puts " "
