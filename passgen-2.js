c9448b86cb3861693e06b25ee35ffcd53bb07756
Sam Detnon <sam.detnon@headlondon.com>
2014-06-20 15:10:26 +0100

    Creating Javascript version

---
+++ passgen-2.js
@@ -0,0 +1,19 @@
+//
+// Password generator
+// By Sam 'phantomofthesoup' Detnon
+//
+
+
+var PHRASES =  [ 'z', 'aw', 'oo', 'ow', 'oi', 'nk', 'ng', 'wh', 'th', 'sh', 'ch', 'str', 'a', 'e', 'i', 'o', 'u', '!', '%', '&', '£', '$']
+
+
+var generate(length){
+	var result = ''
+
+	while(result.length < length){
+		// * ADD * random selection over the array
+		//phra = 
+
+	}
+
+}
