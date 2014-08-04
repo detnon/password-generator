79b2e2e7a93c9bf5c53fee9b140d4e721762e307
Sam Detnon <sam.detnon@headlondon.com>
2014-08-04 16:29:47 +0100

    Can now alter the length of the password

--- background.js
+++ background.js
@@ -1,33 +1,32 @@
 var PHRASES =  []
 
+// gets stings from the file and uses them to generate a password
+var getStrings = function(){
+  $.get('strings.txt', function(data){
+    PHRASES = data.split(' ');
+    $('#pw').text(generate(8))
+    console.log(data);
+    
 
+  })
+}
+
+//Takes an input of length and generates a password 
 var generate = function(length){
   var result = ''
   while(result.length < length){
     // random selection over the array
-    var phra = PHRASES[Math.floor(Math.random()*PHRASES.length)]; //(internet copypasta)
+    var phra = PHRASES[Math.floor(Math.random()*PHRASES.length)];// Math that handles the random password generation
     result += phra
   }
-
   result = result.substring(0,length) //trundicate string to ensure password length is the same as the user input
   return result 
 }
 
 
-var getStrings = function(){
-  $.get('sounds.txt', function(data){
-    console.log(data);
-    PHRASES = data.split(' ');
-    $('#pw').text(generate(8) )
-
-  })
-}
 // JQUERY BEGINS HERE \\
 
-
 //Generate text on page load
-
-
 $(document).ready(function(){
   getStrings();
   
@@ -38,6 +37,12 @@ $(document).ready(function(){
   $('.more').click(function(){
     $('.pick').slideToggle('fast');
   });
+
+  $('#form-id').submit(function(e){ 
+  e.preventDefault(); 
+  var length = $("#pass-length").val();
+  $('#pw').text(generate(length));
+ });
 
 })
 
